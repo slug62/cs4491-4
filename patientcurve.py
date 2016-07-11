91c0b8e00cd8a97a9823f201cda91634fb3e5570
Peter <ptc62@msn.com>
2016-07-11 12:41:53 -0400

    complete

--- patientcurve.py
+++ patientcurve.py
@@ -3,12 +3,13 @@
     Program:        patientcurve.py
     Author:         Peter Southwick
     Date:           07/11/16
-    Description:    A Short program that .
+    Description:    A Short program that takes provided patient data, creates a polynomial from it and then
+                    evaluates the polynomial for intermediate values.
 
 """
 import numpy as np
 import matplotlib.pyplot as plot
-import tabulate as tab
+from tabulate import tabulate
 
 
 def factors(marray):  # Compute factors in marray
@@ -19,43 +20,34 @@ def factors(marray):  # Compute factors in marray
     return mf
 
 providedpatientdata = np.array([5500, 8500, 13500, 20500, 29500, 40500, 53500, 68500])
+provideddatayears = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002]
 
-tempFactors = factors(providedpatientdata)  # Calculate the factors from the given temp data
-firstDiff = np.diff(providedpatientdata)  # Calculate the first difference
-secondDiff = np.diff(firstDiff)  # Calculate the second difference
-tempMean = np.mean(tempFactors)  # Calculate the mean of the factors
-
-calculatedTemps = np.zeros(providedpatientdata.size)
-calculatedTemps[0] = providedpatientdata[0]
-for i in range(providedpatientdata.size - 1):  # Calculate our estimated temp totals using the factor mean
-    calculatedTemps[i+1] = tempMean * calculatedTemps[i]
-
-xi = 0.0
-xf = 8.0
-sn = np.size(providedpatientdata)
-x = np.linspace(xi, xf, sn)
+# Compute a polynomial from the provided data
+xi = 0.0  # Initial value of X
+xf = 8.0  # Final value of X
+sn = np.size(providedpatientdata)  # How many values of X we want
+x = np.linspace(xi, xf, sn)  # Creating an evenly distributed number of X's between xi, xf
 deg = 3  # Degree of our desired polynomial
-poly = np.polyfit(x, providedpatientdata, deg)
-xc = np.linspace(xi, xf, 30)  # new x points
-yc = np.polyval(poly, xc)  # new y points
+poly = np.polyfit(x, providedpatientdata, deg)  # Computing the polynomail from given data
+xc = np.linspace(xi, xf, 30)  # Creating an evenly distributed number of X's for our computed polynomial
+yc = np.polyval(poly, xc)  # Evaluate our computed polynomial
 
-
+# Plot the graphs of both provided data, and computed data
 f, linearr = plot.subplots(2, sharex=True)
 linearr[0].set_title('Provided data')
 linearr[0].plot(x, providedpatientdata, marker='o', linestyle='-')
-linearr[1].set_title('Calculated data')
+linearr[1].set_title('Computed data')
 linearr[1].plot(xc, yc, marker='o', linestyle='-')
 plot.show()
 
+# Create an easy to read table from data of evaluated polynomial
+tableheaders = ['Computed X', 'Computed Y']
+data = []
+for i in range(len(xc)):
+    data.append([xc[i], yc[i]])
 
-
+# Output
 print("Original list:", providedpatientdata)
-print("First diff:", firstDiff)
-print("Second diff:", secondDiff)
-print("Factors:", tempFactors)
-print("Mean:", tempMean)
-print("Calculated Temps using the mean of factors: ", calculatedTemps)
 print("Our PolyFit Coefficients:", poly)
-print("Evaluation of our curve fitted polynomial")
-for j in range(30):
-    print(xc[j], yc[j])
+print("Computing intermediate values by evaluating our computed polynomial")
+print(tabulate(data, headers=tableheaders, tablefmt='fancy_grid', floatfmt='.4f'))
